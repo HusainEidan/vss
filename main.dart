510e4043edd7f8a5a05e1207d3c9af89285fc4ee
HusainEidan <h.m.abbas1999@gmail.com>
2024-09-30 00:38:20 -0700

    convert frome f to c

---
+++ main.dart
@@ -0,0 +1,9 @@
+void main() {
+double tempFahrenheit = 86;
+double tempInCelsius = 30 ;
+
+print("The weather in Celsius is ${(tempFahrenheit-tempInCelsius)/1.8} ");
+
+print("The weather in Celsius is ${tempInCelsius*1.8+32}");
+
+}
